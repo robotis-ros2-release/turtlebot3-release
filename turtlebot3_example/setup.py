34d68e205e934a419da25af8c76959daadac3cc2
Pyo <pyo@robotis.com>
2025-04-01 16:01:38 +0900

    Revert "Trimmed the branch to only the turtlebot3_teleop sub directory"
    This reverts commit 92626554b06aca69cbbcc414c95952a7b3dc6edf.

---
+++ turtlebot3_example/setup.py
@@ -0,0 +1,56 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'turtlebot3_example'
+
+setup(
+    name=package_name,
+    version='2.2.5',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # To be added
+        # ('share/' + package_name + '/launch', glob.glob(os.path.join('launch',
+        #                                                 'turtlebot3_interactive_marker.launch.py'))),
+        # ('share/' + package_name + '/launch', glob.glob(os.path.join('launch',
+        #                                                 'turtlebot3_obstacle_detection.launch.py'))),
+        # ('share/' + package_name + '/rviz', glob.glob(os.path.join('rviz',
+        #                                               'turtlebot3_interactive_marker.rviz'))),
+    ],
+    install_requires=['setuptools', 'launch'],
+    zip_safe=True,
+    author=['Wonho Yun', 'Junyeong Jeong', 'Jeonggeun Lim', 'Will Son', 'Ryan Shim', 'Gilbert'],
+    author_email=['ywh@robotis.com', 'junyeong4321@gmail.com', 'ljg@robotis.com',
+                  'willson@robotis.com', 'jhshim@robotis.com', 'kkjong@robotis.com'],
+    maintainer='Pyo',
+    maintainer_email='pyo@robotis.com',
+    keywords=['ROS', 'ROS2', 'examples', 'rclpy'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'Examples of Different TurtleBot3 Usage.'
+    ),
+    license='Apache License, Version 2.0',
+    entry_points={
+        'console_scripts': [
+            'turtlebot3_interactive_marker = '
+            'turtlebot3_example.turtlebot3_interactive_marker.'
+            'turtlebot3_interactive_marker:main',
+            'turtlebot3_patrol_server = \
+                turtlebot3_example.turtlebot3_patrol.turtlebot3_patrol_server:main',
+            'turtlebot3_patrol_client = \
+                turtlebot3_example.turtlebot3_patrol.turtlebot3_patrol_client:main',
+            'turtlebot3_obstacle_detection = '
+            'turtlebot3_example.turtlebot3_obstacle_detection.'
+            'turtlebot3_obstacle_detection:main',
+            'turtlebot3_position_control_pointop_key = '
+            'turtlebot3_example.turtlebot3_position_control.'
+            'turtlebot3_position_control_pointop_key:main'
+        ],
+    },
+)
